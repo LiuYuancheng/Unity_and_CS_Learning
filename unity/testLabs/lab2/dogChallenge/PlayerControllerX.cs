5458499f8ef31b2ae58e888f262ded56c2bf75d9
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-03-06 14:54:01 +0800

    Update the dog challenge .

---
+++ unity/testLabs/lab2/dogChallenge/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public int keyCount; 
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            keyCount +=1;
+            if(keyCount == 2){
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                keyCount = 0;
+            }
+        }
+    }
+}
---
+++ unity/testLabs/lab2/dogChallenge/SpawnManagerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -22;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    private float startDelay = 1.0f;
+    private float spawnInterval = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomBall", startDelay, Random.Range(3,spawnInterval));
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIdx = Random.Range(0,3);
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIdx], spawnPos, ballPrefabs[ballIdx].transform.rotation);
+    }
+
+}
