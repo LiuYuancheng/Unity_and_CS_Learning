119b780ae3d88b7d2b9f6f75eebb533ad77b195b
LiuYuancheng <liu_yuan_cheng@hotmail.com>
2023-04-11 18:00:01 +0800

    Added the repeat background script.

--- unity/testLabs/lab3/PlayerController.cs
+++ unity/testLabs/lab3/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityModifier = 1f;
     public bool isOnGround = true;
-
+    public bool gameOver = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -28,6 +28,12 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision collision){
-        isOnGround = true;
+        
+        if(collision.gameObject.CompareTag("Ground")){
+            isOnGround = true;
+        }else if(collision.gameObject.CompareTag("Obstacle")){
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
     }
 }
